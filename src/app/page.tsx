e4f316fd47b15323259a96fe74bbd16f096f6dee
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-22 17:21:25 -0400

    creating button functionality for toggling overlay with corresponding form (login/signup)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import Image from "next/image";
+import styles from "../styles/homepage.module.css";
+import { useEffect, useState } from "react";
+import { Overlay } from "@/components/Overlay";
+import { Login } from "@/components/Login";
+import { Signup } from "@/components/Signup";
+
+export default function Homepage() {
+  const [loginIsOpen, setLogin] = useState<boolean>(false)
+  const [signupIsOpen, setSignup] = useState<boolean>(false)
+
+  const openLogin = () => {
+    setLogin(true)
+  }
+
+  const openSignup = () => {
+    setSignup(true)
+  }
+
   return (
      <div className={styles.homepage}>
         <div className={styles.slide1}>
@@ -11,8 +28,8 @@ export default function Home() {
               <h1 className={styles.logo_text}><span className={styles.piggy}>piggy</span><span className={styles.bank}>bank</span></h1>
             </div>
             <div className={styles.buttons}>
-              <button className={styles.loginButton}>Login</button>
-              <button className={styles.signupButton}>Sign Up</button>
+              <button className={styles.loginButton} onClick={openLogin}>Login</button>
+              <button className={styles.signupButton} onClick={openSignup}>Sign Up</button>
             </div>
           </div>
           <div className={styles.main_content}>
@@ -24,6 +41,14 @@ export default function Home() {
             <img className={styles.piggy_bank_lady} src="/lady_holding_piggy_bank.png"></img>
           </div>
         </div>
+        {loginIsOpen && (<>
+              <Overlay close={() => setLogin(false)}/>
+              <Login close={() => setLogin(false)}/>
+        </>)}
+        {signupIsOpen && (<>
+              <Overlay close={() => setSignup(false)}/>
+              <Signup close={() => setSignup(false)}/>
+        </>)}
      </div>
-  );
+  )
 }
